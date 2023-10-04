26cb9be54f37593eb23516d1aed19ff09a70cd24
Duy Vo <duybpz@gmail.com>
2023-10-04 14:23:51 +0700

    [U] Fix map navigator

---
+++ src/navigation/collaborator/HomeStack/MainTabs/MapTab.tsx
@@ -0,0 +1,13 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+import Map from '../../../../screens/collaborator/Map';
+
+const MapTab = () => {
+  return (
+    <Map />
+  )
+}
+
+export default MapTab;
+
+const styles = StyleSheet.create({})
