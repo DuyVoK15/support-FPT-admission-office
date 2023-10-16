e23d3507ce3b8bc8535f401e4514e49c215990c7
Duy Vo <duybpz@gmail.com>
2023-10-16 10:43:51 +0700

    [U] Add more reducers

--- src/app/store.ts
+++ src/app/store.ts
@@ -2,16 +2,22 @@
 
 import { configureStore } from '@reduxjs/toolkit';
 import { useDispatch } from 'react-redux';
-import authReducer from '../features/collaborator/authSlice';
-import postReducer from '../features/collaborator/postSlice';
-import accountReducer from'../features/collaborator/accountSlice';
+import collab_authReducer from '../features/collaborator/collab.authSlice';
+import collab_postReducer from '../features/collaborator/collab.postSlice';
+import collab_postRegistrationReducer from '../features/collaborator/collab.postRegistrationSlice';
+import collab_accountReducer from'../features/collaborator/collab.accountSlice';
+import collab_certificateReducer from '../features/collaborator/collab.certificateSlice';
+import admission_authReducer from '../features/admission/admission.authSlice';
 
 export const store = configureStore({
   reducer: {
-    auth: authReducer,
-    post: postReducer,
-    account: accountReducer,
+    collab_auth: collab_authReducer,
+    collab_post: collab_postReducer,
+    collab_account: collab_accountReducer,
+    collab_postRegistration: collab_postRegistrationReducer,
+    collab_certificate: collab_certificateReducer,
     // Thêm reducers khác nếu cần thiết
+    admission_auth: admission_authReducer, 
   },
 });
 
