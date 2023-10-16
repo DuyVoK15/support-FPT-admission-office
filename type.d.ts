e550887c9e551db68665e982fd71c5c481ae099d
Duy Vo <duybpz@gmail.com>
2023-10-16 10:42:19 +0700

    [U] Config params list of navigation

--- type.d.ts
+++ type.d.ts
@@ -48,3 +48,52 @@ export type HomeCollaboratorScreenNavigationProp = NativeStackScreenProps<
   SECURITY,
   USER_PROFILE_DISABLE
 >;
+
+export type HomeAdmissionStackNavigatorParamList = {
+  HOME: undefined;
+  HOME_TAB: undefined;
+  HOME_STACK_NAVIGATOR: undefined;
+  MAP: undefined;
+  MAP_STACK_NAVIGATOR: undefined;
+  BOOKING: undefined;
+  BOOKING_STACK_NAVIGATOR: undefined;
+  ACCOUNT: undefined;
+  ACCOUNT_STACK_NAVIGATOR: undefined;
+  EVENT: undefined;
+  EVENT_STACK_NAVIGATOR: undefined;
+  EVENT_DETAIL: undefined;
+  PROFILE: undefined;
+  PROFILE_SIGNUP: undefined;
+  CERTIFICATE_HISTORY: undefined;
+  WALLET: undefined;
+  NOTIFICATION: undefined;
+  VERIFICATION: undefined;
+  POSITION_REGISTRATION: undefined;
+  SECURITY: undefined;
+  USER_PROFILE_DISABLE: undefined;
+};
+
+export type HomeAdmissionScreenNavigationProp = NativeStackScreenProps<
+  HomeAdmissionScreenNavigationProp,
+  HOME,
+  HOME_TAB,
+  HOME_STACK_NAVIGATOR,
+  POSITION_REGISTRATION,
+  EVENT,
+  EVENT_STACK_NAVIGATOR,
+  EVENT_DETAIL,
+  MAP,
+  MAP_STACK_NAVIGATOR,
+  BOOKING,
+  BOOKING_STACK_NAVIGATOR,
+  ACCOUNT,
+  ACCOUNT_STACK_NAVIGATOR,
+  PROFILE,
+  PROFILE_SIGNUP,
+  CERTIFICATE_HISTORY,
+  WALLET,
+  NOTIFICATION,
+  VERIFICATION,
+  SECURITY,
+  USER_PROFILE_DISABLE
+>;
