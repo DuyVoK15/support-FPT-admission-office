9d6f368489e877117d91b35139ed0a84782e1af6
Duy Vo <duybpz@gmail.com>
2023-10-20 20:58:02 +0700

    [U] Fix map

--- src/screens/collaborator/Map/index.tsx
+++ src/screens/collaborator/Map/index.tsx
@@ -1,29 +1,67 @@
-import { StyleSheet, Text, View } from 'react-native';
-import React from 'react';
-import MapView from 'react-native-maps';
+import React, { useState, useEffect } from 'react';
+import { Platform, Text, View, StyleSheet } from 'react-native';
+import Device from 'expo-device';
+import * as Location from 'expo-location';
+import MapView, { Marker } from 'react-native-maps';
 
-const Map = () => {
+export default function Map() {
+  const [location, setLocation] = useState<Location.LocationObject | null>(
+    null
+  );
+  const [errorMsg, setErrorMsg] = useState<string | null>(null);
+
+  useEffect(() => {
+    (async () => {
+      if (Platform.OS === 'android' && !Device.isDevice) {
+        setErrorMsg(
+          'Oops, this will not work on Snack in an Android Emulator. Try it on your device!'
+        );
+        return;
+      }
+      let { status } = await Location.requestForegroundPermissionsAsync();
+      if (status !== 'granted') {
+        setErrorMsg('Permission to access location was denied');
+        return;
+      }
+
+      let location = await Location.getCurrentPositionAsync({});
+      setLocation(location);
+      console.log(location);
+    })();
+  }, []);
+
+  let text = 'Waiting...';
+  if (errorMsg) {
+    text = errorMsg;
+  } else if (location) {
+    text = JSON.stringify(location);
+  }
+
   return (
     <View style={styles.container}>
       <MapView
         initialRegion={{
           latitude: 37.78825,
           longitude: -122.4324,
-          latitudeDelta: 0.0922,
-          longitudeDelta: 0.0421,
+          latitudeDelta: 0.01,
+          longitudeDelta: 0.01,
         }}
         style={styles.map}
-      />
+      >
+        <Marker
+          coordinate={{ latitude: 37.78825 , longitude: -122.4324 }}
+          image={require('../../../assets/Images/ic_location.png')}
+        />
+      </MapView>
     </View>
   );
-};
-
-export default Map;
+}
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
   },
+
   map: {
     width: '100%',
     height: '100%',
