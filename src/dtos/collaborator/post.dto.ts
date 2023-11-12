bda520fc3b874918e53b7d36196a681112f2f60b
Duy Vo <duybpz@gmail.com>
2023-11-12 15:48:00 +0700

    [U] FIx dto post

--- src/dtos/collaborator/post.dto.ts
+++ src/dtos/collaborator/post.dto.ts
@@ -1,9 +1,9 @@
-import DataPost from '../../models/collaborator/dataPost.model';
+import { DataPost } from '../../models/collaborator/dataPost.model';
 import MetaDataPost from '../../models/collaborator/metaDataPost.model';
 
 export default interface PostDto {
   metadata: MetaDataPost;
-  data: DataPost | [];
+  data: DataPost[] | [];
   isError: boolean;
   message: string;
 }
