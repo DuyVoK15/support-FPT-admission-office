e8776dd21a1a7df25c22ff360400a38bc8d6ed41
Duy Vo <duybpz@gmail.com>
2023-11-13 23:53:33 +0700

    [U] Add postUpcommingKeySearch state in Context

--- src/context/stateContext.tsx
+++ src/context/stateContext.tsx
@@ -5,17 +5,36 @@ export type MyContextProps = {
   setPostReOpenCategoryId: (id: number | null) => void;
   postUpcommingCategoryId: number | null;
   setPostUpcommingCategoryId: (id: number | null) => void;
-}
+  postUpcommingKeySearch: string | null;
+  setPostUpcommingKeySearch: (keySearch: string | null) => void;
+};
 // Tạo một context mới
 export const MyContext = createContext<MyContextProps | null>(null);
 
 // Provider component
-export const MyContextProvider: React.FC<{children: ReactNode}> = ({ children }) => {
-  const [postReOpenCategoryId, setPostReOpenCategoryId ] = useState<number | null>(null);
-  const [postUpcommingCategoryId, setPostUpcommingCategoryId ] = useState<number | null>(null);
-
+export const MyContextProvider: React.FC<{ children: ReactNode }> = ({
+  children,
+}) => {
+  const [postReOpenCategoryId, setPostReOpenCategoryId] = useState<
+    number | null
+  >(null);
+  const [postUpcommingCategoryId, setPostUpcommingCategoryId] = useState<
+    number | null
+  >(null);
+  const [postUpcommingKeySearch, setPostUpcommingKeySearch] = useState<
+    string | null
+  >(null);
   return (
-    <MyContext.Provider value={{postReOpenCategoryId, postUpcommingCategoryId, setPostReOpenCategoryId , setPostUpcommingCategoryId}} >
+    <MyContext.Provider
+      value={{
+        postReOpenCategoryId,
+        postUpcommingCategoryId,
+        postUpcommingKeySearch,
+        setPostReOpenCategoryId,
+        setPostUpcommingCategoryId,
+        setPostUpcommingKeySearch,
+      }}
+    >
       {children}
     </MyContext.Provider>
   );
