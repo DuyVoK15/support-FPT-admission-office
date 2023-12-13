b364ec0797bce4dbe3b34b84be61599e9f1e11f7
DuyVoK15 <duybpz@gmail.com>
2023-12-13 11:31:07 +0700

    [U] Fix map Carousel

--- src/screens/collaborator/Map/Map.tsx
+++ src/screens/collaborator/Map/Map.tsx
@@ -7,10 +7,13 @@ import {
   Button,
   NativeModules,
   FlatList,
+  Linking,
+  TouchableOpacity,
+  Image,
 } from 'react-native';
 import Device from 'expo-device';
 import * as Location from 'expo-location';
-import MapView, { Marker, PROVIDER_GOOGLE } from 'react-native-maps';
+import MapView, { Callout, Marker, PROVIDER_GOOGLE } from 'react-native-maps';
 import { GooglePlacesAutocomplete } from 'react-native-google-places-autocomplete';
 import { GOOGLE_API_KEY } from '../../../../env';
 import { ScreenHeight, ScreenWidth } from '../../../constants/Demesions';
@@ -30,8 +33,28 @@ const statusBarHeight = Constants.statusBarHeight;
 import { FlashList } from '@shopify/flash-list';
 import { SHADOWS } from '../../../constants/Shadows';
 import Carousel from 'react-native-reanimated-carousel';
+import { FONTS_FAMILY } from '../../../constants/Fonts';
+import { imageNotFoundUri } from '../../../utils/images';
+import { COLORS } from '../../../constants/Colors';
+import RegistrationStatus from '../Registration/RegistrationStatus';
+import Animated, {
+  Extrapolate,
+  interpolate,
+  useAnimatedStyle,
+  useSharedValue,
+} from 'react-native-reanimated';
+import { transformer } from '../../../../metro.config';
+import { Entypo } from '@expo/vector-icons';
+
+interface ItemCarouselProps {
+  item?: DataViewPostRegistration;
+  index: number;
+  scrollX: number;
+}
 
 export default function Map() {
+  const [scrollX, setScrollX] = useState(0);
+
   const [location, setLocation] = useState<Location.LocationObject | null>(
     null
   );
@@ -63,6 +86,7 @@ export default function Map() {
   // }
 
   const mapRef = useRef<MapView>(null);
+  const markerRefs = useRef<any>([]);
   const [selectedMarkerIndex, setSelectedMarkerIndex] = useState<number>(-1); // Default selected marker ID
 
   const handleMarkerPress = (index: number) => {
@@ -73,14 +97,12 @@ export default function Map() {
       await fetchPostRegistrationConfirmed();
     };
     fetchData();
-    
   }, []);
   useEffect(() => {
     setTimeout(() => {
       handleMarkerPress(0);
-
-    },2000)
-  },[])
+    }, 2000);
+  }, []);
   useEffect(() => {
     // Here you can handle moving the map to the selected marker's coordinates
     const selectedMarker = postRegistrationConfirmList?.data?.find(
@@ -98,6 +120,14 @@ export default function Map() {
           latitudeDelta: 0.01,
           longitudeDelta: 0.01,
         });
+
+        if (
+          markerRefs &&
+          markerRefs.current[selectedMarkerIndex] &&
+          markerRefs.current[selectedMarkerIndex].showCallout
+        ) {
+          markerRefs.current[selectedMarkerIndex].showCallout();
+        }
       }
     }
   }, [selectedMarkerIndex]);
@@ -108,6 +138,8 @@ export default function Map() {
   const fetchPostRegistrationConfirmed = async () => {
     await dispatch(
       getAllPostRegistration_Confirmed({
+        Sort: 'CreateAt',
+        Order: 'DESCENDING',
         RegistrationStatus: [REGISTRATION_STATUS_ENUM.CONFIRM],
       })
     ).then((res) => {
@@ -128,6 +160,7 @@ export default function Map() {
       return (
         <Marker
           key={index}
+          ref={(ref) => (markerRefs.current[index] = ref)}
           coordinate={{
             latitude: registration?.postPosition?.latitude
               ? Number(registration?.postPosition?.latitude)
@@ -138,32 +171,110 @@ export default function Map() {
           }}
           title={registration?.post?.postCategory?.postCategoryDescription}
           description={registration?.postPosition?.positionDescription}
-          onPress={() => handleMarkerPress(registration?.id)}
-        />
+          // onPress={(event) => console.log(event)}
+          isPreselected={true}
+          onCalloutPress={() =>
+            openGoogleMaps(
+              registration?.postPosition?.latitude
+                ? registration?.postPosition?.latitude
+                : '0',
+              registration?.postPosition?.longitude
+                ? registration?.postPosition?.longitude
+                : '0'
+            )
+          }
+        >
+          <Callout
+            onPress={() =>
+              openGoogleMaps(
+                registration?.postPosition?.latitude
+                  ? registration?.postPosition?.latitude
+                  : '0',
+                registration?.postPosition?.longitude
+                  ? registration?.postPosition?.longitude
+                  : '0'
+              )
+            }
+            style={{ backgroundColor: '#FFF', padding: 10, borderRadius: 10 }}
+          >
+            <View style={{ borderRadius: 10 }}>
+              <View style={{ alignItems: 'center' }}>
+                <Text
+                  style={{
+                    fontFamily: FONTS_FAMILY?.Ubuntu_700Bold,
+                    fontSize: 18,
+                  }}
+                >
+                  {registration?.post?.postCategory?.postCategoryDescription
+                    ? registration?.post?.postCategory?.postCategoryDescription
+                    : 'No value'}
+                </Text>
+              </View>
+              <View style={{ marginTop: 10 }}>
+                <Text
+                  style={{
+                    fontFamily: FONTS_FAMILY?.Ubuntu_400Regular,
+                    fontSize: 14,
+                  }}
+                >
+                  Position:
+                  <Text
+                    style={{
+                      fontFamily: FONTS_FAMILY?.Ubuntu_500Medium,
+                      fontSize: 14,
+                    }}
+                  >
+                    {' '}
+                    {registration?.postPosition?.positionName
+                      ? registration?.postPosition?.positionName
+                      : 'No value'}
+                  </Text>
+                </Text>
+              </View>
+              <View style={{ marginTop: 10 }}>
+                <Text
+                  style={{
+                    fontFamily: FONTS_FAMILY?.Ubuntu_400Regular,
+                    fontSize: 14,
+                  }}
+                >
+                  School:
+                  <Text
+                    style={{
+                      fontFamily: FONTS_FAMILY?.Ubuntu_500Medium,
+                      fontSize: 14,
+                    }}
+                  >
+                    {' '}
+                    {registration?.postPosition?.schoolName
+                      ? registration?.postPosition?.schoolName
+                      : 'No value'}
+                  </Text>
+                </Text>
+              </View>
+
+              <TouchableOpacity style={{ alignItems: 'center', marginTop: 10 }}>
+                <Text
+                  style={{
+                    fontFamily: FONTS_FAMILY?.Ubuntu_700Bold,
+                    fontSize: 14,
+                    color: '#4287f5',
+                    textDecorationLine: 'underline',
+                  }}
+                >
+                  Click to Show On GoogleMap
+                </Text>
+              </TouchableOpacity>
+            </View>
+          </Callout>
+        </Marker>
       );
     });
   };
 
+  const onRegionChangeComplete = () => {};
+
   const carouselRef = useRef<any>(null);
-  const renderItemCarousel = ({ item }: { item: DataViewPostRegistration }) => {
-    return (
-      <View
-        style={{
-          // flex: 1,
-          // marginLeft: ScreenWidth * 0.05,
-          // marginRight: ScreenWidth * 0.05,
-          // marginBottom: 20,
-          backgroundColor: 'white',
-          borderRadius: 10,
-          ...SHADOWS.SHADOW_04,
-        }}
-      >
-        <View style={{ margin: 15 }}>
-          <Text>{item?.registrationCode}</Text>
-        </View>
-      </View>
-    );
-  };
 
   const regionTS = {
     latitude: 10.74325841775703,
@@ -172,9 +283,137 @@ export default function Map() {
     longitudeDelta: 3.957008980214596,
   };
 
+  const openGoogleMaps = (latitude: string, longitude: string) => {
+    const url = `https://www.google.com/maps?q=${latitude},${longitude}`;
+    Linking.openURL(url);
+  };
+
+  const renderCarouselItem = ({ item }: { item: DataViewPostRegistration }) => {
+    return (
+      <View
+        style={[
+          {
+            backgroundColor: 'transparent',
+            width: ScreenWidth,
+            justifyContent: 'center',
+            alignItems: 'center',
+            // marginLeft: index === 0 ? SIDE_CARD_LENGTH : SPACING,
+            // marginRight: index === 2 ? SIDE_CARD_LENGTH : SPACING,
+          },
+          // cardStyle,
+        ]}
+      >
+        <View
+          style={{
+            width: ScreenWidth * 0.9,
+            borderRadius: 15,
+            // height: ScreenWidth * 0.3,
+            backgroundColor: 'white',
+          }}
+        >
+          <View style={{ margin: 15 }}>
+            <View style={{ flexDirection: 'row', alignItems: 'center' }}>
+              <View
+                style={{
+                  flex: 0,
+                  borderRadius: 10,
+                }}
+              >
+                <Image
+                  style={{
+                    height: ScreenWidth * 0.2,
+                    width: ScreenWidth * 0.3,
+                    resizeMode: 'cover',
+                    borderRadius: 10,
+                  }}
+                  source={{
+                    uri: item?.post?.postImg
+                      ? item?.post?.postImg
+                      : imageNotFoundUri,
+                  }}
+                />
+              </View>
+              <View style={{ flex: 1, marginLeft: 15 }}>
+                <Text
+                  style={{
+                    fontFamily: FONTS_FAMILY.Ubuntu_500Medium,
+                    fontSize: 13,
+                    color: COLORS.light_black,
+                  }}
+                >
+                  General
+                </Text>
+                <Text
+                  style={{
+                    fontFamily: FONTS_FAMILY.Ubuntu_500Medium,
+                    fontSize: 16,
+                    color: 'black',
+                    marginTop: 5,
+                  }}
+                >
+                  {item?.post?.postCategory?.postCategoryDescription
+                    ? item?.post?.postCategory?.postCategoryDescription
+                    : 'No value'}
+                </Text>
+                <Text
+                  style={{
+                    fontFamily: FONTS_FAMILY?.Ubuntu_400Regular,
+                    fontSize: 12,
+                    color: COLORS?.light_grey,
+                    marginTop: 5,
+                  }}
+                >
+                  {item?.registrationCode
+                    ? 'PRCode: ' + item?.registrationCode
+                    : 'No value'}
+                </Text>
+              </View>
+            </View>
+            <RegistrationStatus status={REGISTRATION_STATUS_ENUM.CONFIRM} />
+            <View style={{ marginTop: 15, alignItems: 'flex-start' }}>
+              <TouchableOpacity
+              onPress={() =>
+                openGoogleMaps(
+                  item?.postPosition?.latitude
+                    ? item?.postPosition?.latitude
+                    : '0',
+                    item?.postPosition?.longitude
+                    ? item?.postPosition?.longitude
+                    : '0'
+                )}
+                style={{
+                  flexDirection: 'row',
+                  alignItems: 'center',
+                  paddingVertical: 12,
+                  paddingHorizontal: 18,
+                  borderRadius: 15,
+                  backgroundColor: COLORS?.blue_status,
+                  justifyContent: 'space-evenly',
+                }}
+              >
+                <View>
+                  <Text
+                    style={{
+                      fontFamily: FONTS_FAMILY?.Ubuntu_500Medium,
+                      color: '#FFF',
+                    }}
+                  >
+                    Show on Google Map
+                  </Text>
+                </View>
+                <View style={{ marginLeft: 5 }}>
+                  <Entypo name="direction" size={24} color="white" />
+                </View>
+              </TouchableOpacity>
+            </View>
+          </View>
+        </View>
+      </View>
+    );
+  };
+
   return (
     <View style={styles.container}>
-      <StatusBar backgroundColor="transparent" />
       <MapView
         key={markersKey.toString()}
         provider={PROVIDER_GOOGLE}
@@ -194,27 +433,37 @@ export default function Map() {
         // followsUserLocation={true}
         customMapStyle={[]}
         // onMapLoaded={(map) => console.log(map)}
-
-        onRegionChange={(region) => console.log(region)}
+        moveOnMarkerPress={false}
+        // onRegionChange={(region) => console.log(region)}
+        // onRegionChangeComplete={onRegionChangeComplete}
       >
         {postRegistrationConfirmList && renderMarkers()}
       </MapView>
-      <View style={{}}>
-        {postRegistrationConfirmList &&
-          postRegistrationConfirmList?.data?.length > 0 && (
-            <Carousel
-              style={{}}
-              loop
-              width={ScreenWidth}
-              height={ScreenWidth / 2}
-              // autoPlay={true}
-              data={postRegistrationConfirmList?.data}
-              scrollAnimationDuration={200}
-              onSnapToItem={(index) => handleMarkerPress(index)}
-              renderItem={renderItemCarousel}
-            />
-          )}
+      <View style={{ position: 'absolute', bottom: 20 }}>
+        <FlatList
+          style={{}}
+          horizontal
+          showsHorizontalScrollIndicator={false}
+          decelerationRate={'fast'}
+          snapToInterval={ScreenWidth}
+          snapToAlignment="center"
+          pagingEnabled={true}
+          data={postRegistrationConfirmList?.data}
+          onScroll={(event) => {
+            const offset = event.nativeEvent.contentOffset.x;
+
+            const index = Math.floor(offset / (ScreenWidth - 50));
+            handleMarkerPress(index);
+            console.log(index);
+          }}
+          scrollEventThrottle={0}
+          renderItem={renderCarouselItem}
+          keyExtractor={(item, index) => index.toString()}
+        />
       </View>
+      {/* <View>
+        <Button title="Redirect" onPress={openGoogleMaps} />
+      </View> */}
 
       {/* <View
         style={{
