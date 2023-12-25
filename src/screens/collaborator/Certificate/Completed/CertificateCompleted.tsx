99bcc1a17f2a40ec9a6a1bd813a96f27a97916a6
DuyVoK15 <duybpz@gmail.com>
2023-12-25 22:57:49 +0700

    [U] Fix Certificate

--- src/screens/collaborator/Certificate/Completed/CertificateCompleted.tsx
+++ src/screens/collaborator/Certificate/Completed/CertificateCompleted.tsx
@@ -1,62 +1,78 @@
 import {
+  Animated,
+  FlatList,
   RefreshControl,
   ScrollView,
   StyleSheet,
   Text,
   View,
 } from 'react-native';
-import React from 'react';
+import React, { useEffect, useRef, useState } from 'react';
 import CertificateCard from '../../../../components/collaborator/Certificate/CertificateCard';
-import useCompletedCertificate from './useCertificateCompleted';
 import { format_ISODateString_To_DDMMYYYY } from '../../../../utils/formats';
+import CERTIFICATE_STATUS_ENUM from '../../../../enums/collaborator/CertificateStatus';
+import RegistrationEmpty from '../../../../components/shared/Empty/RegistrationEmpty';
+import { CertificateData } from '../../../../models/collaborator/certificate.model';
+import { useRoute } from '@react-navigation/native';
+import { HomeCollaboratorScreenNavigationProp } from '../../../../../type';
+import { SHADOWS } from '../../../../constants/Shadows';
+import { COLORS } from '../../../../constants/Colors';
+import { ScreenWidth } from '../../../../constants/Demesions';
+import useCompletedCertificate from './useCertificateCompleted';
 
 const Certificate_Completed_Status = () => {
-  const { props, handlers, state } = useCompletedCertificate();
+  const { state, props, handlers } = useCompletedCertificate();
 
+  const renderEmptyComponent = () => {
+    return <RegistrationEmpty />;
+  };
+  const renderItem = ({ item }: { item: CertificateData }) => {
+    return (
+      <View style={[styles.containerItem]}>
+        <CertificateCard
+          dateReceive={
+            item?.createAt
+              ? format_ISODateString_To_DDMMYYYY(item?.createAt)
+                ? format_ISODateString_To_DDMMYYYY(item?.createAt)
+                : 'No date'
+              : 'No date'
+          }
+          certificateID={
+            item?.id ? String(item?.trainingCertificate?.id) : 'No value'
+          }
+          certificateName={
+            item?.trainingCertificate?.certificateName
+              ? item?.trainingCertificate?.certificateName
+              : 'No value'
+          }
+          confirmBy={
+            item?.certificateIssuer?.name
+              ? item?.certificateIssuer?.name
+              : 'System'
+          }
+          status={
+            item?.status === CERTIFICATE_STATUS_ENUM.COMPLETED
+              ? 'Completed'
+              : 'Rejected'
+          }
+        />
+      </View>
+    );
+  };
   return (
     <View style={styles.container}>
-      <View style={{ marginBottom: 30 }}>
-        <ScrollView
+      <View style={{ flex: 1 }}>
+        <FlatList
+          data={props.certificateList?.data}
+          renderItem={renderItem}
           refreshControl={
             <RefreshControl
               refreshing={state.refreshing}
               onRefresh={handlers.onRefresh}
             />
           }
-        >
-          {props?.certificateList?.data ? (
-            props?.certificateList?.data?.map((certificate, index) => (
-              <CertificateCard
-                key={index}
-                dateReceive={
-                  certificate?.createAt
-                    ? format_ISODateString_To_DDMMYYYY(certificate?.createAt)
-                      ? format_ISODateString_To_DDMMYYYY(certificate?.createAt)
-                      : 'No date'
-                    : 'No date'
-                }
-                certificateID={
-                  certificate?.id
-                    ? String(certificate?.trainingCertificate?.id)
-                    : 'No value'
-                }
-                certificateName={
-                  certificate?.trainingCertificate?.certificateName
-                    ? certificate?.trainingCertificate?.certificateName
-                    : 'No value'
-                }
-                confirmBy={
-                  certificate?.certificateIssuer?.name
-                    ? certificate?.certificateIssuer?.name
-                    : 'System'
-                }
-                status={'Completed'}
-              />
-            ))
-          ) : (
-            <View />
-          )}
-        </ScrollView>
+          ListEmptyComponent={renderEmptyComponent}
+        />
       </View>
     </View>
   );
@@ -68,5 +84,14 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: 'white',
+  },
+  containerItem: {
+    marginVertical: 10,
+    marginHorizontal: 15,
+    backgroundColor: 'white',
+    borderTopRightRadius: 20,
+    borderBottomRightRadius: 20,
+    borderBottomLeftRadius: 20,
+    ...SHADOWS.SHADOW_06
   },
 });
--- src/screens/collaborator/Certificate/Rejected/CertificateRejected.tsx
+++ src/screens/collaborator/Certificate/Rejected/CertificateRejected.tsx
@@ -1,57 +1,78 @@
-import { RefreshControl, ScrollView, StyleSheet, Text, View } from 'react-native';
-import React from 'react';
+import {
+  Animated,
+  FlatList,
+  RefreshControl,
+  ScrollView,
+  StyleSheet,
+  Text,
+  View,
+} from 'react-native';
+import React, { useEffect, useRef, useState } from 'react';
 import CertificateCard from '../../../../components/collaborator/Certificate/CertificateCard';
-import useRejectedCertificate from './useCertificateRejected';
 import { format_ISODateString_To_DDMMYYYY } from '../../../../utils/formats';
+import CERTIFICATE_STATUS_ENUM from '../../../../enums/collaborator/CertificateStatus';
+import RegistrationEmpty from '../../../../components/shared/Empty/RegistrationEmpty';
+import { CertificateData } from '../../../../models/collaborator/certificate.model';
+import { useRoute } from '@react-navigation/native';
+import { HomeCollaboratorScreenNavigationProp } from '../../../../../type';
+import { SHADOWS } from '../../../../constants/Shadows';
+import { COLORS } from '../../../../constants/Colors';
+import { ScreenWidth } from '../../../../constants/Demesions';
+import useRejectedCertificate from './useCertificateRejected';
 
 const Certificate_Rejected_Status = () => {
   const { state, props, handlers } = useRejectedCertificate();
 
+  const renderEmptyComponent = () => {
+    return <RegistrationEmpty />;
+  };
+  const renderItem = ({ item }: { item: CertificateData }) => {
+    return (
+      <View style={[styles.containerItem]}>
+        <CertificateCard
+          dateReceive={
+            item?.createAt
+              ? format_ISODateString_To_DDMMYYYY(item?.createAt)
+                ? format_ISODateString_To_DDMMYYYY(item?.createAt)
+                : 'No date'
+              : 'No date'
+          }
+          certificateID={
+            item?.id ? String(item?.trainingCertificate?.id) : 'No value'
+          }
+          certificateName={
+            item?.trainingCertificate?.certificateName
+              ? item?.trainingCertificate?.certificateName
+              : 'No value'
+          }
+          confirmBy={
+            item?.certificateIssuer?.name
+              ? item?.certificateIssuer?.name
+              : 'System'
+          }
+          status={
+            item?.status === CERTIFICATE_STATUS_ENUM.COMPLETED
+              ? 'Completed'
+              : 'Rejected'
+          }
+        />
+      </View>
+    );
+  };
   return (
     <View style={styles.container}>
-      <View style={{ marginBottom: 30 }}>
-        <ScrollView
+      <View style={{ flex: 1 }}>
+        <FlatList
+          data={props.certificateList?.data}
+          renderItem={renderItem}
           refreshControl={
             <RefreshControl
               refreshing={state.refreshing}
               onRefresh={handlers.onRefresh}
             />
           }
-        >
-          {props?.certificateList?.data ? (
-            props?.certificateList?.data
-              .map((certificate, index) => (
-                <CertificateCard
-                key={index}
-                dateReceive={
-                  certificate?.createAt
-                    ? format_ISODateString_To_DDMMYYYY(certificate?.createAt)
-                      ? format_ISODateString_To_DDMMYYYY(certificate?.createAt)
-                      : 'No date'
-                    : 'No date'
-                }
-                certificateID={
-                  certificate?.id
-                    ? String(certificate?.trainingCertificate?.id)
-                    : 'No value'
-                }
-                certificateName={
-                  certificate?.trainingCertificate?.certificateName
-                    ? certificate?.trainingCertificate?.certificateName
-                    : 'No value'
-                }
-                confirmBy={
-                  certificate?.certificateIssuer?.name
-                    ? certificate?.certificateIssuer?.name
-                    : 'System'
-                }
-                status={'Rejected'}
-              />
-              ))
-          ) : (
-            <View />
-          )}
-        </ScrollView>
+          ListEmptyComponent={renderEmptyComponent}
+        />
       </View>
     </View>
   );
@@ -63,5 +84,14 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: 'white',
+  },
+  containerItem: {
+    marginVertical: 10,
+    marginHorizontal: 15,
+    backgroundColor: 'white',
+    borderTopRightRadius: 20,
+    borderBottomRightRadius: 20,
+    borderBottomLeftRadius: 20,
+    ...SHADOWS.SHADOW_06
   },
 });
